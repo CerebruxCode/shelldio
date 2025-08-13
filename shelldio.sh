0949d39dc92333659ad6d8ee948ecc33e75cdf99
Salih Emin <914656+cerebrux@users.noreply.github.com>
2025-08-13 14:56:10 +0000

    unused loop variable should be _

--- shelldio.sh
+++ shelldio.sh
@@ -93,7 +93,7 @@ start_mpv() {
   mpv_pid=$!
 
   # Wait for the IPC socket to be ready (max 2 seconds)
-  for i in {1..40}; do
+for _ in {1..40}; do
     if [ -S /tmp/mpv_socket ]; then
       break
     fi
